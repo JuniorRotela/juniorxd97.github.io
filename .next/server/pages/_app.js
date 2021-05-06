a0eac401eb7ac0de4db4a5f4a2c1f5ad0a81aee2
juniorxd97 <juniorrotela@hotmail.com>
2021-05-05 20:18:04 -0400

    initial commit

--- .next/server/pages/_app.js
+++ .next/server/pages/_app.js
@@ -131,7 +131,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _global_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../global.css */ "./global.css");
 /* harmony import */ var _global_css__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_global_css__WEBPACK_IMPORTED_MODULE_2__);
 
-var _jsxFileName = "C:\\Users\\Mcbook\\Desktop\\Portafolio\\pages\\_app.js";
+var _jsxFileName = "C:\\Users\\Mcbook\\Desktop\\Portafolio\\juniorxd97.github.io\\pages\\_app.js";
 
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
